2fa5fd7396613dce96c36d9b74d32596ee851298
Mark Saunders <mark@misterhoots.com>
2021-10-21 22:10:16 +0000

    commit

--- startup.sh
+++ startup.sh
@@ -9,3 +9,10 @@ sudo apt install ansible -y
 apt-get install google-drive-ocamlfuse -y
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+# Cleans cached apt pkgs
+sudo apt-get clean
+# remove old kernels
+sudo apt-get autoremove --purge
+# Removed pkgs that are dependancies of other uninstalled pkgs
+sudo apt-get autoremove
+
