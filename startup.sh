1c3405b27ad5141f29136549f1741d0059499584
Mark Saunders <mark@misterhoots.com>
2021-10-21 21:24:48 +0000

    update all

--- startup.sh
+++ startup.sh
@@ -3,5 +3,9 @@ apt-get upgrade -y
 apt-add-repository ppa:ansible/ansible -y
 add-apt-repository ppa:alessandro-strada/ppa -y
 apt-get update
+# Ansible
 sudo apt install ansible -y
+# Google Drive Setup
 apt-get install google-drive-ocamlfuse -y
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
