9ed3232c565b6e8ed4823de4c638dbe0d4ba2c95
Mark Saunders <mark@misterhoots.com>
2021-10-21 19:46:46 +0000

    update to startup scripts

---
+++ startup.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get upgrade -y
+apt-add-repository ppa:ansible/ansible -y
+add-apt-repository ppa:alessandro-strada/ppa -y
+apt-get update
+sudo apt install ansible -y
+apt-get install google-drive-ocamlfuse -y
